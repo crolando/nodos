54b075f3894356199c6009c7b49c5eb46535d18b
Charles <mummylauncher@gmail.com>
2021-04-17 12:21:05 -0400

    Add attr_table class to give basic attribute has/get/set

--- attribute.h
+++ attribute.h
@@ -14,9 +14,27 @@ enum attr_type { string, fnumber, inumber};
 class attr_value
 {
 public:
+    // Default constructor so I can use map["blah"] = newValue;
+    attr_value(){};
+    attr_value(const attr_string& Value) {
+        s = Value;
+        t = string;
+    };
+
+    attr_value(const attr_fnumber Value) {
+        f = Value;
+        t = fnumber;
+    }
+
+    attr_value(const attr_inumber Value) {
+        i = Value;
+        t = inumber;
+    }
+
     void set(const attr_string& value) {s = value;}
     void set(const double value) {f = value;};
     void set(const long value) {f = value;};
+
     attr_type get_type(void) {return t;};
     attr_string& get_string(void) {return s;};
     attr_fnumber get_float(void) {return f;};
@@ -28,6 +46,51 @@ private:
     attr_inumber    i = 0;
 };
 
-typedef std::map <attr_name, attr_value> attr_table;
+class attr_table {
+public:
+    bool has_attr(const attr_name& Key)
+    {
+        std::map <attr_name, attr_value>::iterator it;
+        it = table.find(Key);
+        if (it == table.end()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    void set_attr(const attr_name& Key, attr_string& Value) {
+        if(has_attr(Key) == false) {
+            table[Key] = attr_value(Value);
+        } else {
+            table[Key].set(Value);
+        }
+    }
+
+    void set_attr(const attr_name& Key, attr_fnumber& Value) {
+        if(has_attr(Key) == false) {
+            table[Key] = attr_value(Value);
+        } else {
+            table[Key].set(Value);
+        }
+    }
+
+    void set_attr(const attr_name& Key, attr_inumber& Value) {
+        if(has_attr(Key) == false) {
+            table[Key] = attr_value(Value);
+        } else {
+            table[Key].set(Value);
+        }
+    }
+
+    const attr_value& get_attr(const attr_name& Key) {
+        return table[Key];
+    }
+
+
+private:
+    std::map <attr_name, attr_value> table;
+};
+
 
 #endif // ATTRIBUTE_H
